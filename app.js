333a089fb577baccebbcb8cd332462f8794e2c4d
Terry Allitt <tallitt88@gmail.com>
2018-03-06 23:39:49 +0000

    Install yargs module to parse command line arguments for basic note functionality e.g. add, list, read, and remove

--- app.js
+++ app.js
@@ -2,5 +2,23 @@ console.log("Starting App");
 
 const fs = require('fs');
 const os = require('os');
+const yargs = require('yargs');
 
 const notes = require('./notes.js')
+
+const argv = yargs.argv;
+var command = argv._[0];
+console.log('Command: ', command);
+console.log('Yargs', argv);
+
+if (command === 'add') {
+  notes.addNote(argv.title, argv.body);
+} else if (command === 'list') {
+  notes.getAll();
+} else if (command === 'read') {
+  notes.getNote(argv.title);
+} else if (command === 'remove') {
+  notes.removeNote(argv.title);
+} else {
+  console.log('Not recognised');
+};
