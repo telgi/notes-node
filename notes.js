2bc3fab34837f45c5a283ffb2377f3c57cfd72b8
Terry Allitt <tallitt88@gmail.com>
2018-03-07 22:42:26 +0000

    Build on addNote function by using an array to push notes into and using fs module to read and write to json file

--- notes.js
+++ notes.js
@@ -1,7 +1,27 @@
 console.log('Starting notes.js');
 
+const fs = require('fs');
+
 var addNote = (title, body) => {
-  console.log('Adding note', title, body);
+  var notes = [];
+  var note = {
+    title,
+    body
+  };
+
+  try {
+    var noteString = fs.readFileSync('notes-data.json');
+    notes = JSON.parse(noteString);
+  } catch (e) {
+
+  }
+
+  duplicateNotes = notes.filter((note) => note.title === title);
+
+  if (duplicateNotes.length === 0) {
+    notes.push(note);
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+  };
 };
 
 var getAll = () => {
